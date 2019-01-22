0586609094058d76eb5077d99842d964445a3e9d
Eugene Biletskiy <ebeletskiy@gmail.com>
2019-01-22 16:38:53 +0100

    Add initial intent.

--- app/src/main/java/com/testlab/yevhenbiletskiy/testlab/ui/main/MainIntent.kt
+++ app/src/main/java/com/testlab/yevhenbiletskiy/testlab/ui/main/MainIntent.kt
@@ -2,4 +2,7 @@ package com.testlab.yevhenbiletskiy.testlab.ui.main
 
 import com.testlab.yevhenbiletskiy.testlab.mvi.MviIntent
 
-sealed class MainIntent : MviIntent
+sealed class MainIntent : MviIntent {
+
+  object InitialIntent : MainIntent()
+}
