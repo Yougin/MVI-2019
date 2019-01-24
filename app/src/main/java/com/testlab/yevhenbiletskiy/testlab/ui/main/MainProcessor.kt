f6e9bc7290be6c70045848b985fa0492a85244b2
Eugene Biletskiy <ebeletskiy@gmail.com>
2019-01-24 21:22:43 +0100

    Update MainProcessor.kt

--- app/src/main/java/com/testlab/yevhenbiletskiy/testlab/ui/main/MainProcessor.kt
+++ app/src/main/java/com/testlab/yevhenbiletskiy/testlab/ui/main/MainProcessor.kt
@@ -1,16 +1,17 @@
 package com.testlab.yevhenbiletskiy.testlab.ui.main
 
+import io.reactivex.Observable
 import io.reactivex.ObservableTransformer
 
 object MainProcessor {
   val process = ObservableTransformer<MainAction, MainResult> { upstream ->
     upstream.publish { shared ->
-      shared.ofType(MainAction.InitialLoadAction::class.java).compose(onInitialLoadAction())
+      shared.ofType(MainAction.InitialLoadAction::class.java).onInitialLoadAction()
     }
   }
 
-  private fun onInitialLoadAction() =
-      ObservableTransformer<MainAction, MainResult> {
-        it.map { MainResult.InitialLoadResult("Hello World!") }
-      }
+  private fun Observable<out MainAction>.onInitialLoadAction(): Observable<MainResult> {
+    return map { MainResult.InitialLoadResult("Hello World!") }
+  }
+
 }
