b635c7b8dbc3d185609efc24650988919bc61c25
Eugene Biletskiy <ebeletskiy@gmail.com>
2019-01-24 21:19:41 +0100

    Extract ObservableTransformer from processor.

--- app/src/main/java/com/testlab/yevhenbiletskiy/testlab/ui/main/MainProcessor.kt
+++ app/src/main/java/com/testlab/yevhenbiletskiy/testlab/ui/main/MainProcessor.kt
@@ -3,9 +3,14 @@ package com.testlab.yevhenbiletskiy.testlab.ui.main
 import io.reactivex.ObservableTransformer
 
 object MainProcessor {
-  val process = ObservableTransformer<MainAction, MainResult.InitialLoadResult> { upstream ->
+  val process = ObservableTransformer<MainAction, MainResult> { upstream ->
     upstream.publish { shared ->
-      shared.ofType(MainAction.InitialLoadAction::class.java).compose { it.map { MainResult.InitialLoadResult("afd") } }
+      shared.ofType(MainAction.InitialLoadAction::class.java).compose(onInitialLoadAction())
     }
   }
+
+  private fun onInitialLoadAction() =
+      ObservableTransformer<MainAction, MainResult> {
+        it.map { MainResult.InitialLoadResult("Hello World!") }
+      }
 }
