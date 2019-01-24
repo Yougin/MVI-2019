cad26f86be253963abd5ee77977b5e219fe9ba7c
Eugene Biletskiy <ebeletskiy@gmail.com>
2019-01-24 21:10:47 +0100

    Move to MainState

--- app/src/main/java/com/testlab/yevhenbiletskiy/testlab/ui/main/MainState.kt
+++ app/src/main/java/com/testlab/yevhenbiletskiy/testlab/ui/main/MainState.kt
@@ -1,5 +1,6 @@
 package com.testlab.yevhenbiletskiy.testlab.ui.main
 
+import com.testlab.yevhenbiletskiy.testlab.mvi.MviIntent
 import com.testlab.yevhenbiletskiy.testlab.mvi.MviState
 
 data class MainState(
@@ -13,4 +14,17 @@ data class MainState(
         text = ""
     )
   }
+}
+
+sealed class MainIntent : MviIntent {
+  object InitialIntent : MainIntent()
+}
+
+sealed class MainAction {
+  object InitialLoadAction : MainAction()
+}
+
+sealed class MainResult {
+  // TODO-eugene why to use constructor MainResult()
+  data class InitialLoadResult(val text: String) : MainResult()
 }
