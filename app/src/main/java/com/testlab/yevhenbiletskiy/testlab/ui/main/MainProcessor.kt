74c86140e2a00acbe5edb8fdf712b2d6e5f03f96
Eugene Biletskiy <ebeletskiy@gmail.com>
2019-01-28 15:53:37 +0100

    Return multiple results from onFetchDataAction().

--- app/src/main/java/com/testlab/yevhenbiletskiy/testlab/ui/main/MainProcessor.kt
+++ app/src/main/java/com/testlab/yevhenbiletskiy/testlab/ui/main/MainProcessor.kt
@@ -1,18 +1,21 @@
 package com.testlab.yevhenbiletskiy.testlab.ui.main
 
+import android.annotation.SuppressLint
 import com.testlab.yevhenbiletskiy.testlab.domain.Lce
 import io.reactivex.Observable
 import io.reactivex.ObservableTransformer
 
 object MainProcessor {
-  val process = ObservableTransformer<MainAction, Lce<out MainResult>> { upstream ->
-    upstream.publish { shared ->
-      shared.ofType(MainAction.FetchDataAction::class.java).onFetchDataAction()
+    val process = ObservableTransformer<MainAction, Lce<out MainResult>> { upstream ->
+        upstream.publish<Lce<out MainResult>> { shared ->
+            shared.ofType(MainAction.FetchDataAction::class.java).onFetchDataAction()
+        }
     }
-  }
 
-  private fun Observable<out MainAction>.onFetchDataAction(): Observable<Lce<out MainResult>> {
-    return map { Lce.Content(MainResult.InitialLoadResult("Hello World!")) }
-  }
-
+    private fun Observable<out MainAction>.onFetchDataAction(): Observable<Lce<out MainResult>> =
+        flatMap {
+            Observable
+                .just<Lce<MainResult.InitialLoadResult>>(Lce.Content(MainResult.InitialLoadResult("Hello World!")))
+                .startWith(Lce.Loading())
+        }
 }
