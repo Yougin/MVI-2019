4364a3a6fdc400218d0c7eb00fa51933329e9108
Eugene Biletskiy <ebeletskiy@gmail.com>
2019-02-05 15:08:53 +0100

    Minor update.

--- app/src/main/java/com/testlab/yevhenbiletskiy/testlab/ui/main/MainFragment.kt
+++ app/src/main/java/com/testlab/yevhenbiletskiy/testlab/ui/main/MainFragment.kt
@@ -30,9 +30,7 @@ class MainFragment : Fragment() {
   override fun onCreateView(
       inflater: LayoutInflater, container: ViewGroup?,
       savedInstanceState: Bundle?
-  ): View {
-    return inflater.inflate(R.layout.main_fragment, container, false)
-  }
+  ): View = inflater.inflate(R.layout.main_fragment, container, false)
 
   override fun onActivityCreated(savedInstanceState: Bundle?) {
     super.onActivityCreated(savedInstanceState)
