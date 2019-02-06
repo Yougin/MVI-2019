bb699386a32e2b2f88c4294231985189df234486
Eugene Biletskiy <ebeletskiy@gmail.com>
2019-02-06 20:23:40 +0100

    ViewModule as part of subcomponent object graph.

--- app/src/main/java/com/testlab/yevhenbiletskiy/testlab/AppSetup.kt
+++ app/src/main/java/com/testlab/yevhenbiletskiy/testlab/AppSetup.kt
@@ -18,8 +18,7 @@ annotation class MainFragmentScope
 
 @Singleton
 @Component(modules = [
-  AppModule::class,
-  ViewModelModule::class
+  AppModule::class
 ])
 interface AppComponent {
   fun inject(app: App)
@@ -32,48 +31,4 @@ class AppModule(private val app: Context) {
   @Provides
   @Singleton
   fun provideApplicationContext(): Context = app
-}
-
-
-// View Model injection part
-// https://github.com/googlesamples/android-architecture-components/blob/master/GithubBrowserSample/app/src/main/java /com/android/example/github/di/ViewModelModule.kt
-// https://www.techyourchance.com/dependency-injection-viewmodel-with-dagger-2/
-
-@MustBeDocumented
-@Target(
-    AnnotationTarget.FUNCTION,
-    AnnotationTarget.PROPERTY_GETTER,
-    AnnotationTarget.PROPERTY_SETTER
-)
-@Retention(AnnotationRetention.RUNTIME)
-@MapKey
-annotation class ViewModelKey(val value: KClass<out ViewModel>)
-
-@Module
-abstract class ViewModelModule {
-
-  @Binds
-  @IntoMap
-  @ViewModelKey(MainViewModel::class)
-  abstract fun bindUserViewModel(mainViewModel: MainViewModel): ViewModel
-
-  @Binds
-  abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
-}
-
-@Singleton
-class ViewModelFactory @Inject constructor(
-    private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
-) : ViewModelProvider.Factory {
-  override fun <T : ViewModel> create(modelClass: Class<T>): T {
-    val creator = creators[modelClass] ?: creators.entries.firstOrNull {
-      modelClass.isAssignableFrom(it.key)
-    }?.value ?: throw IllegalArgumentException("unknown model class $modelClass")
-    try {
-      @Suppress("UNCHECKED_CAST")
-      return creator.get() as T
-    } catch (e: Exception) {
-      throw RuntimeException(e)
-    }
-  }
 }
--- app/src/main/java/com/testlab/yevhenbiletskiy/testlab/ui/main/MainFragmentSetup.kt
+++ app/src/main/java/com/testlab/yevhenbiletskiy/testlab/ui/main/MainFragmentSetup.kt
@@ -1,12 +1,64 @@
 package com.testlab.yevhenbiletskiy.testlab.ui.main
 
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
+import dagger.Binds
+import dagger.MapKey
 import dagger.Module
 import dagger.Subcomponent
+import dagger.multibindings.IntoMap
+import javax.inject.Inject
+import javax.inject.Provider
+import kotlin.reflect.KClass
 
 @Subcomponent(modules = [(MainFragmentModule::class)])
 interface MainFragmentComponent {
   fun inject(activity: MainFragment)
 }
 
-@Module()
-class MainFragmentModule
+@Module(includes = [ViewModelModule::class])
+abstract class MainFragmentModule {
+
+  @Binds
+  @IntoMap
+  @ViewModelKey(MainViewModel::class)
+  abstract fun bindUserViewModel(mainViewModel: MainViewModel): ViewModel
+
+}
+
+// View Model injection part
+// https://github.com/googlesamples/android-architecture-components/blob/master/GithubBrowserSample/app/src/main/java /com/android/example/github/di/ViewModelModule.kt
+// https://www.techyourchance.com/dependency-injection-viewmodel-with-dagger-2/
+
+@MustBeDocumented
+@Target(
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER
+)
+@Retention(AnnotationRetention.RUNTIME)
+@MapKey
+annotation class ViewModelKey(val value: KClass<out ViewModel>)
+
+@Module
+abstract class ViewModelModule {
+
+  @Binds
+  abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
+}
+
+class ViewModelFactory @Inject constructor(
+    private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
+) : ViewModelProvider.Factory {
+  override fun <T : ViewModel> create(modelClass: Class<T>): T {
+    val creator = creators[modelClass] ?: creators.entries.firstOrNull {
+      modelClass.isAssignableFrom(it.key)
+    }?.value ?: throw IllegalArgumentException("unknown model class $modelClass")
+    try {
+      @Suppress("UNCHECKED_CAST")
+      return creator.get() as T
+    } catch (e: Exception) {
+      throw RuntimeException(e)
+    }
+  }
+}
