704dcc1e2c27f0cbc5e5d76ef62be620ee7afb31
Eugene Biletskiy <ebeletskiy@gmail.com>
2019-02-07 12:02:12 +0100

    Split some files.

--- app/src/main/java/com/testlab/yevhenbiletskiy/testlab/presentation/di/viewmodel/VewModelModule.kt
+++ app/src/main/java/com/testlab/yevhenbiletskiy/testlab/presentation/di/viewmodel/VewModelModule.kt
@@ -1,25 +1,12 @@
 package com.testlab.yevhenbiletskiy.testlab.presentation.di.viewmodel
 
-import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
 import dagger.Binds
-import dagger.MapKey
 import dagger.Module
-import kotlin.reflect.KClass
 
 // View Model injection part
 // https://github.com/googlesamples/android-architecture-components/blob/master/GithubBrowserSample/app/src/main/java /com/android/example/github/di/ViewModelModule.kt
 // https://www.techyourchance.com/dependency-injection-viewmodel-with-dagger-2/
-
-@MustBeDocumented
-@Target(
-    AnnotationTarget.FUNCTION,
-    AnnotationTarget.PROPERTY_GETTER,
-    AnnotationTarget.PROPERTY_SETTER
-)
-@Retention(AnnotationRetention.RUNTIME)
-@MapKey
-annotation class ViewModelKey(val value: KClass<out ViewModel>)
 
 @Module
 abstract class ViewModelModule {
