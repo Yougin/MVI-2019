19f46ca10eab438b9819d051669e0682d8a55402
Eugene Biletskiy <ebeletskiy@gmail.com>
2019-02-13 16:07:40 +0100

    Formatting update.

--- app/src/main/java/com/testlab/yevhenbiletskiy/testlab/presentation/screens/main/MainProcessor.kt
+++ app/src/main/java/com/testlab/yevhenbiletskiy/testlab/presentation/screens/main/MainProcessor.kt
@@ -4,27 +4,22 @@ import com.testlab.yevhenbiletskiy.testlab.domain.Lce
 import com.testlab.yevhenbiletskiy.testlab.presentation.mvi.Processor
 import io.reactivex.Observable
 import io.reactivex.ObservableTransformer
-import io.reactivex.android.schedulers.AndroidSchedulers
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class MainProcessor @Inject constructor() : Processor<MainAction, Lce<out MainResult>> {
 
-  override fun process() = ObservableTransformer<MainAction, Lce<out MainResult>> { upstream ->
-    upstream.publish<Lce<out MainResult>> { shared ->
-      shared.ofType(MainAction.FetchDataAction::class.java).onFetchDataAction()
+    override fun process() = ObservableTransformer<MainAction, Lce<out MainResult>> { upstream ->
+        upstream.publish<Lce<out MainResult>> { shared ->
+            shared.ofType(MainAction.FetchDataAction::class.java).onFetchDataAction()
+        }
     }
-  }
 
-  private fun Observable<out MainAction>.onFetchDataAction(): Observable<Lce<out MainResult>> =
-      flatMap {
-        Observable
-            .just<Lce<MainResult.InitialLoadResult>>(
-                Lce.Content(
-                    MainResult.InitialLoadResult("Hello World!")
-                )
-            )
-            .delay(3, TimeUnit.SECONDS)
-            .startWith(Lce.Loading())
-      }
+    private fun Observable<out MainAction>.onFetchDataAction(): Observable<Lce<out MainResult>> =
+        flatMap {
+            Observable
+                .just<Lce<MainResult.InitialLoadResult>>(Lce.Content(MainResult.InitialLoadResult("Hello World!")))
+                .delay(3, TimeUnit.SECONDS)
+                .startWith(Lce.Loading())
+        }
 }
