f6df650cd102b744d04c6bd66cbea6906cbc7e05
Eugene Biletskiy <ebeletskiy@gmail.com>
2019-02-18 14:29:39 +0100

    Use BehaviorSubject to simplify ViewState's properties.

--- app/src/main/java/com/testlab/yevhenbiletskiy/testlab/presentation/screens/main/MainViewModel.kt
+++ app/src/main/java/com/testlab/yevhenbiletskiy/testlab/presentation/screens/main/MainViewModel.kt
@@ -5,22 +5,41 @@ import com.testlab.yevhenbiletskiy.testlab.domain.Lce
 import com.testlab.yevhenbiletskiy.testlab.presentation.mvi.MviViewModel
 import com.testlab.yevhenbiletskiy.testlab.presentation.mvi.Processor
 import io.reactivex.Observable
+import io.reactivex.ObservableTransformer
 import io.reactivex.disposables.Disposable
+import io.reactivex.subjects.BehaviorSubject
 import io.reactivex.subjects.PublishSubject
 import timber.log.Timber
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor(
-    private val processor: Processor<MainIntent, Lce<out MainResult>>
+    processor: Processor<MainIntent, Lce<out MainResult>>
 ) : ViewModel(),
     MviViewModel<MainIntent, MainState> {
 
   private val intentsEmitter = PublishSubject.create<MainIntent>()
-
-  private val _viewStateEmitter = stream()
-  override fun viewState(): Observable<MainState> = _viewStateEmitter
+  private val _viewState = BehaviorSubject.create<MainState>()
+  override fun viewState(): Observable<MainState> = _viewState
 
   private var disposable: Disposable? = null
+
+  init {
+    val viewChanges = intentsEmitter
+        .takeInitialIntentOnlyOnce()
+        .doOnNext { Timber.d("----- Intent: ${it.javaClass.simpleName}") }
+        .compose(processor.process())
+        .doOnNext { Timber.d("----- Result: ${it.javaClass.simpleName}") }
+        .publish()
+
+    viewChanges.compose(resultToViewState()).subscribe(_viewState)
+
+    viewChanges.autoConnect(0) { disposable = it }
+  }
+
+  private fun resultToViewState() =
+      ObservableTransformer<Lce<out MainResult>, MainState> { upstream ->
+        upstream.scan(MainState.idle(), MainReducer.reduce()).distinctUntilChanged()
+      }
 
   override fun intents(intents: Observable<MainIntent>): Disposable =
       intents.subscribe(
@@ -28,15 +47,6 @@ class MainViewModel @Inject constructor(
           { Timber.e(it, "Something went wrong processing intents") }
       )
 
-  private fun stream() = intentsEmitter
-      .takeInitialIntentOnlyOnce()
-      .doOnNext { Timber.d("----- Intent: ${it.javaClass.simpleName}") }
-      .compose(processor.process())
-      .doOnNext { Timber.d("----- Result: ${it.javaClass.simpleName}") }
-      .scan(MainState.idle(), MainReducer.reduce())
-      .distinctUntilChanged()
-      .replay(1)
-      .autoConnect(0) { disposable = it }
 
   private fun Observable<MainIntent>.takeInitialIntentOnlyOnce() =
       compose { upstream ->
