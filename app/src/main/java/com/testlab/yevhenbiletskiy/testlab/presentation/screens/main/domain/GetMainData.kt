fc1a3ca00cbae970a36bc89a152f1aa6169178d9
Eugene Biletskiy <ebeletskiy@gmail.com>
2019-02-21 15:01:04 +0100

    Leave comment.

--- app/src/main/java/com/testlab/yevhenbiletskiy/testlab/presentation/screens/main/domain/GetMainData.kt
+++ app/src/main/java/com/testlab/yevhenbiletskiy/testlab/presentation/screens/main/domain/GetMainData.kt
@@ -4,6 +4,8 @@ import arrow.core.Option
 import io.reactivex.Observable
 import javax.inject.Inject
 
+// TODO-eugene Just trying packaging by feature, not completely sold on keeping use cases so
+// close to presentation yet, but let's give it a try.
 interface GetMainData {
   operator fun invoke(): Observable<Option<MainText>>
 }
