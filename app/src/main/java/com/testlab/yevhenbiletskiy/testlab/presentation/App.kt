08de737834a86da63b498570c020e371b466df5e
Eugene Biletskiy <ebeletskiy@gmail.com>
2019-03-04 14:35:36 +0100

    leave todo.

--- app/src/main/java/com/testlab/yevhenbiletskiy/testlab/presentation/App.kt
+++ app/src/main/java/com/testlab/yevhenbiletskiy/testlab/presentation/App.kt
@@ -6,7 +6,8 @@ import timber.log.Timber.DebugTree
 
 
 open class App : Application() {
-  // TODO-eugene make it accessible like in https://medium.com/androiddevelopers/dependency-injection-in-a-multi-module-project-1a09511c14b7
+  // TODO-eugene make it accessible like using companion object like it is made in
+  // https://medium.com/androiddevelopers/dependency-injection-in-a-multi-module-project-1a09511c14b7
   open lateinit var component: AppComponent
 
   override fun onCreate() {
